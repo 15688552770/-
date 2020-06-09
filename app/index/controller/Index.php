d38f28d7badc6e08b91e52de5be87eb7ce107ecd
15688552770 <54655005+15688552770@users.noreply.github.com>
2020-06-09 14:25:15 +0800

    Add files via upload

---
+++ app/index/controller/Index.php
@@ -0,0 +1,19 @@
+<?php
+namespace app\index\controller;
+
+use app\BaseController;
+
+class Index extends BaseController
+{
+    public function index()
+    {
+       
+
+     return view();
+    }
+
+    public function hello($name = 'ThinkPHP6')
+    {
+        return 'hello,' . $name;
+    }
+}
